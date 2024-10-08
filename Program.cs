27a515aa873a80ec731c0c69017ce7ee202fae7e
OscarSystemDeveloper <oscj87@gmail.com>
2024-10-08 11:46:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GroupProjectBiblotek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to group 4 biblotek projekt!");
+        }
+    }
+}
